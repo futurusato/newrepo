36bf0ac9f5fa3dbbe6a747918af6e243d3feac7f
futurusato <annaildio1@gmail.com>
2026-02-18 14:16:55 +0300

    Add files via upload

--- get-dac/8-bit-dac-manual.py
+++ get-dac/8-bit-dac-manual.py
@@ -1,26 +1,25 @@
-import RPi.GPIO as gpio
-gpio.setmode(gpio.BCM)
-pins = [22,27,17,26,25,21,20,16]
-gpio.setup(pins, gpio.OUT)
-dynamic_range = 3.3
-def voltage_to_number(voltage):
-    if not (0.0 <= voltage<= dynamic_range):
-        print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {dynamic_range:.2f} B)")
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+outp=[16,20,21,25,26,17,27,22]
+GPIO.setup(outp,GPIO.OUT)
+dyn_range=float(input())
+def voltage_to_nomber(voltage):
+    if not (0.0<=voltage<=dyn_range):
+        print(f"Напряжение выходит за динамический диапазон ЦАП (0.00- {dyn_range:.2f} В)")
         print("Устанавливаем 0.0 В")
         return 0
 
-        return int(voltage / dynamic_range * 255)
+    return int(voltage/dyn_range*255)
 def number_to_dac(value):
-    gpio.output(pins, [int(element) for element in bin(value)[2:].zfill(8)])
+    GPIO.output(outp,[int(element) for element in bin(value)[2:].zfill(8)])
 try:
     while True:
         try:
-            voltage = float(input("Введите напряжение в Вольтах:"))
-            number = voltage_to_number(voltage)
+            voltage=float(input("Введите напряжение в вольтах:"))
+            number=voltage_to_nomber(voltage)
             number_to_dac(number)
-
         except ValueError:
-            print("Вы ввели не число. Попробуйте еще раз \n")
+            print("Вы ввели не число.Попробуйте ещё раз\n")
 finally:
-    gpio.output(pins, 0)
-    gpio.cleanup()
+    GPIO.output(outp,0)
+    GPIO.cleanup()
