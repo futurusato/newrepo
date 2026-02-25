76d9c887727eb7d396a81c9ee947bf87a730dfdf
futurusato <annaildio1@gmail.com>
2026-02-25 13:29:34 +0300

    pwm

--- get-dac/pwm_dac.py
+++ get-dac/pwm_dac.py
@@ -1,26 +1,30 @@
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
 class PWM_DAC:
-    def __init__(self, gpio_pin,pwm_frequency,dynamic_range,verbose = False):
+    def __init__(self,gpio_pin,pwm_frequency,dynamic_range,verbose=False):
         self.gpio_pin=gpio_pin
         self.dynamic_range=dynamic_range
         self.verbose=verbose
         self.pwm_frequency=pwm_frequency
 
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.gpio_pin,GPIO.OUT, initial = 0)
-    def deinit(self):
-        GPIO.output(self.gpio_pin,0)
-        GPIO.cleanup()
+        GPIO.setup(self.gpio_pin,GPIO.OUT)
+        self.pwm=GPIO.PWM(self.gpio_pin,self.pwm_frequency)
+        self.pwm.start(0.0)
+    def set_number(self,number):
+       GPIO.output(self.gpio_pin,[int(element) for element in bin(number)[2:].zfill(8)])
     def set_voltage(self,voltage):
        if not (0.0<=voltage<=self.dynamic_range):
            print(f"Напряжение выходит за динамический диапазон ЦАП (0.00- {self.dynamic_range:.2f} В)")
            print("Устанавливаем 0.0 В")
-           return 0
-       GPIO.output(self.gpio_pin,[int(element) for element in bin(int(voltage/self.dynamic_range*255))[2:].zfill(8)])
+           return self.pwm.ChangeDutyCycle (0)
+       duty=int(voltage/self.dynamic_range*100)
+       self.pwm.ChangeDutyCycle(duty)
+    def deinit(self):
+        GPIO.output(self.gpio_pin,0)
+        GPIO.cleanup()
 if  __name__=="__main__":
     try:
-        dac = PWM_DAC(12,500,3.290, True)
+        dac = PWM_DAC(12,1000,3.183, True)
 
         while True:
             try:
