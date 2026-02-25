0a5f678858218333b7b8f88ebe80a29428050030
futurusato <annaildio1@gmail.com>
2026-02-25 14:59:28 +0300

    last three tasks

--- get-dac/pwm_sin.py
+++ get-dac/pwm_sin.py
@@ -1,14 +1,13 @@
-import r2r_dac as r2r
+import pwm_dac as pd
 import signal_generator as sg
 import time
-
-amplitude = 3.2
+amplitude = 3
 signal_frequency=10
 sampling_frequency=1000
 
 if __name__=="__main__":
     try:
-        dac = r2r.R2R_DAC([16,20,21,25,26,17,27,22],3.183, True)
+        dac = pd.PWM_DAC(12,1000,3.3, True)
         start_time=time.time()
         while True:
             current_time=time.time()-start_time
