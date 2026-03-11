d52e8abe863b7907d5c02ab3a663ec8bbb8a0ad5
futurusato <annaildio1@gmail.com>
2026-03-11 13:43:10 +0300

    to seventh task

--- get-adc/r2r-sar.py
+++ get-adc/r2r-sar.py
@@ -1,22 +1,22 @@
-import RPi.GPIO as gpio
-import r2r_adc as r2r
-import adc_plot as graph
 import time
+import adc_plot as graph
+import r2r_adc
 
-adc = r2r.R2R_ADC(3.285,0.0001)
-time_values=[]
+adc = r2r_adc.R2R_ADC(3.285,0.000001)
+
 voltage_values=[]
+time_values=[]
 duration = 3.0
 
 try:
     start=time.time()
     while (time.time()-start)<duration:
         c=time.time()-start
-        vol=adc.get_sc_voltage()
+        vol=adc.get_sar_voltage()
         voltage_values.append(vol)
         time_values.append(c)
     graph.plot_voltage_vs_time(time_values, voltage_values, 3.285)
     graph.plot_sampling_period_hist(time_values)
+
 finally:
         adc.deinit()
-
