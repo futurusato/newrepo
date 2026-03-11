c64aa0596c49635aad42ceaf1727fb48f18ac9ae
futurusato <annaildio1@gmail.com>
2026-03-11 14:54:40 +0300

    Update adc_plot.py

--- get-adc/adc_plot.py
+++ get-adc/adc_plot.py
@@ -1,5 +1,7 @@
 from matplotlib import pyplot as plt
-
+import RPi.GPIO as GPIO
+import time
+import r2r_adc as r2r
 def plot_sampling_period_hist(t):
     sampling_periods=[t[i+1]-t[i] for i in range(len(t)-1)]
     plt.figure(figsize=(10,6))
@@ -18,3 +20,18 @@ def plot_voltage_vs_time(time, voltage, max_voltage):
     plt.ylabel("Напряжение, В")
     plt.grid(True)
     plt.show()
+voltage_values=[]
+time_values=[]
+duration=3.0
+try:
+    dac = r2r.R2R_ADC(3.285,0.000001)
+    start=time.time()
+    while (time.time()-start)<duration:
+        c=time.time()-start
+        vol=dac.get_sc_voltage()
+        voltage_values.append(vol)
+        time_values.append(c)
+    plot_voltage_vs_time(time_values, voltage_values, 3.285)
+    plot_sampling_period_hist(time_values)
+finally:
+        dac.deinit()
